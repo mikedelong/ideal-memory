223e6f8d76c63fe948a5f10d87fe2580effe71ce
mikedelong <mikedelong@outlook.com>
2020-07-27 08:38:23 -0400

    cleanup: put the count vectorizer case before the tf-idf case

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
     differences = list()
     random_states = list(range(1, run_count + 1))
     test_size_ = 0.1
-    which_classifier = 5  # 4 is best so far
+    which_classifier = 5  # need a cook-off to find the best one
     for random_state_ in random_states:
         X_train, X_test, y_train, y_test = train_test_split(train_df['clean'], train_df['Classification'],
                                                             random_state=random_state_, test_size=test_size_, )
@@ -120,20 +120,20 @@ if __name__ == '__main__':
             y_predicted = classifier.predict(X=tfidf_vectorizer.transform(X_test), )
         elif which_classifier == 4:
             # https://towardsdatascience.com/spam-detection-with-logistic-regression-23e3709e522
-            model_name = 'logreg/tf-idf'
-            tfidf_vectorizer = TfidfVectorizer(ngram_range=(1, 3), )
-            counts = tfidf_vectorizer.fit_transform(X_train.values, )
-            classifier = LogisticRegression(penalty='l1', solver='liblinear', )
-            classifier.fit(X=counts, y=y_train.values, )
-            y_predicted = classifier.predict(X=tfidf_vectorizer.transform(X_test), )
-        elif which_classifier == 5:
-            # https://towardsdatascience.com/spam-detection-with-logistic-regression-23e3709e522
             model_name = 'logreg/count'
             count_vectorizer = CountVectorizer(ngram_range=(1, 3), )
             counts = count_vectorizer.fit_transform(X_train.values, )
             classifier = LogisticRegression(penalty='l1', solver='liblinear', )
             classifier.fit(X=counts, y=y_train.values, )
             y_predicted = classifier.predict(X=count_vectorizer.transform(X_test), )
+        elif which_classifier == 5:
+            # https://towardsdatascience.com/spam-detection-with-logistic-regression-23e3709e522
+            model_name = 'logreg/tf-idf'
+            tfidf_vectorizer = TfidfVectorizer(ngram_range=(1, 3), )
+            counts = tfidf_vectorizer.fit_transform(X_train.values, )
+            classifier = LogisticRegression(penalty='l1', solver='liblinear', )
+            classifier.fit(X=counts, y=y_train.values, )
+            y_predicted = classifier.predict(X=tfidf_vectorizer.transform(X_test), )
         elif which_classifier == 6:
             model_name = 'randfor/tf-idf'
             tfidf_vectorizer = TfidfVectorizer(ngram_range=(1, 3), )
