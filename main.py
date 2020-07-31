81580155e4375f7f9f51e7995795d548fbf500bf
mikedelong <mikedelong@outlook.com>
2020-07-31 08:19:38 -0400

    refactor: extract the logistic regression/count vectorizer classifier as a function

--- main.py
+++ main.py
@@ -57,6 +57,16 @@ def bayes_tf_idf(x_train, y, test):
     return 'Bayes/tf-idf', result
 
 
+def logreg_count():
+    # https://towardsdatascience.com/spam-detection-with-logistic-regression-23e3709e522
+    vectorizer = CountVectorizer(ngram_range=(1, 3), )
+    transformed = vectorizer.fit_transform(X_train.values, )
+    local_classifier = LogisticRegression(penalty='l1', solver='liblinear', )
+    local_classifier.fit(X=transformed, y=y_train.values, )
+    result = local_classifier.predict(X=vectorizer.transform(X_test), )
+    return 'logreg/count', result
+
+
 def spam_bow(train, test):
     local_classifier = SpamClassifier(method='bow', grams=1, train_data=train, )
     local_classifier.train()
@@ -137,13 +147,7 @@ if __name__ == '__main__':
             elif which_classifier == 3:
                 model_name, y_predicted = bayes_tf_idf(X_train, y_train, X_test, )
             elif which_classifier == 4:
-                # https://towardsdatascience.com/spam-detection-with-logistic-regression-23e3709e522
-                model_name = 'logreg/count'
-                count_vectorizer = CountVectorizer(ngram_range=(1, 3), )
-                counts = count_vectorizer.fit_transform(X_train.values, )
-                classifier = LogisticRegression(penalty='l1', solver='liblinear', )
-                classifier.fit(X=counts, y=y_train.values, )
-                y_predicted = classifier.predict(X=count_vectorizer.transform(X_test), )
+                model_name, y_predicted = logreg_count(X_train, y_train, X_test, )
             elif which_classifier == 5:
                 # https://towardsdatascience.com/spam-detection-with-logistic-regression-23e3709e522
                 model_name = 'logreg/tf-idf'
