d171c31fe1e99a49152d98b6bb94a549b3d9f3e1
mikedelong <mikedelong@outlook.com>
2020-08-05 08:17:56 -0400

    cleanup: sort parameters alphabetically

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ def adaboost_tf_idf(x_train, y, test, random_state, ):
     vectorizer = TfidfVectorizer(ngram_range=(1, 3), )
     counts = vectorizer.fit_transform(x_train.values, )
     algorithm = 'SAMME.R'  # was 'SAMME.R'
-    learning_rate = 1.0
-    return 'ada/tf-idf', AdaBoostClassifier(algorithm=algorithm, learning_rate=learning_rate, base_estimator=None,
+    learning_rate = 2.0 # was 1.0
+    return 'ada/tf-idf', AdaBoostClassifier(algorithm=algorithm, base_estimator=None, learning_rate=learning_rate,
                                             n_estimators=100, random_state=random_state,
                                             ).fit(X=counts, y=y.values, ).predict(X=vectorizer.transform(test), )
 
