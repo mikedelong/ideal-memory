9165649bd999406c8bd9e6d1719fdfa229cb72e5
mikedelong <mikedelong@outlook.com>
2020-08-05 08:50:54 -0400

    feature: doubling the learning rate degrades performance, so let's try cutting it in half

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def adaboost_tf_idf(x_train, y, test, random_state, ):
     vectorizer = TfidfVectorizer(ngram_range=(1, 3), )
     counts = vectorizer.fit_transform(x_train.values, )
     algorithm = 'SAMME.R'  # was 'SAMME.R'
-    learning_rate = 2.0  # was 1.0
+    learning_rate = 0.5  # was 1.0
     return 'ada/tf-idf', AdaBoostClassifier(algorithm=algorithm, base_estimator=None, learning_rate=learning_rate,
                                             n_estimators=100, random_state=random_state,
                                             ).fit(X=counts, y=y.values, ).predict(X=vectorizer.transform(test), )
